d0da6ec5247977c0c4fc802fb45d2d5b130f6582
Amey Rane <arane@pmaps.in>
2025-10-21 16:45:26 +0530

    Initial commit - setup React frontend and Spring Boot backend

---
+++ backend/src/test/java/com/smartDocAi/backend/SmartDocAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartDocAi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDocAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
