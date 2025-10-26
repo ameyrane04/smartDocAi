f398f884055394d1b2e63d30f2a22b4a8caf9107
Amey Rane <arane@pmaps.in>
2025-10-26 17:40:26 +0530

    tailwind setup completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
+
 function App() {
+  const [count, setCount] = useState(0)
   const [message, setMessage] = useState('Loading...')
 
   useEffect(() => {
     axios.get('http://localhost:8080/hello')
       .then(res => setMessage(res.data))
-      .catch(() => setMessage("error connecting to backend."));
-
+      .catch(() => setMessage('Error connecting to backend'))
   }, [])
 
   return (
     <>
-      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-        <h1>{message}</h1>
+      <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">
+          Tailwind CSS is Working! 🎉
+        </h1>
       </div>
     </>
   )
